3e2bbe52af737e46629709adae1a6443a9d0726b
Sergio Ramirez <srg00017@gmail.com>
2015-05-25 12:27:54 +0200

    Added more examples

---
+++ examples/FStest.scala
@@ -0,0 +1,22 @@
+package examples
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.linalg.Vectors
+import java.util.ArrayList
+import org.apache.spark.mllib.feature._
+
+object FStest {
+  def main(args: Array[String]): Unit = {
+    val initStartTime = System.nanoTime()
+    val conf = new SparkConf().setAppName("FS test")
+    val sc = new SparkContext(conf)
+    val data = MLUtils.loadLibSVMFile(sc, "a2a.libsvm")    
+        val selector = MrmrSelector.train(data)    
+        val redData = data.map { lp => 
+            LabeledPoint(lp.label, selector.transform(lp.features)) 
+        } 
+        println(redData.first().toString())
+  }
+}
